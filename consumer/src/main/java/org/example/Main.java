4c1cd3f5e5b6f5f9fdff204daa6d1eb73729f8e3
VickyD <vicky17d@gmail.com>
2023-01-23 12:44:15 -0800

    Repro 'add dependency' on producer.main module issue

--- consumer/src/main/java/org/example/Main.java
+++ consumer/src/main/java/org/example/Main.java
@@ -1,9 +1,9 @@
 package org.example;
 import org.example.MainApi;
-import org.example.FooApi;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        MainApi mainApi;
     }
 }
